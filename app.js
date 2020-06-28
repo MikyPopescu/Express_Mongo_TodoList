6649855e1db908730683c64aaf4effa216ea51d8
MikyPopescu <mihaellap42@gmail.com>
2020-06-28 14:08:11 +0300

    persistency

--- app.js
+++ app.js
@@ -1,66 +1,65 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs')
-const date = require(__dirname+'/date.js');
+const mongoose = require("mongoose")
 
 const app = express();
 
-const items = ['Go shopping','Do homework','Get the trash'];
 const workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(express.static("public"))
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useUnifiedTopology:true, useNewUrlParser:true});
+
+//schema
+const itemsSchema = {
+    name: String
+};
+
+//model
+const Item = mongoose.model("Item", itemsSchema);
+
+//documents
+const item1 = new Item({
+    name:"Welcome to your todoList"
+});
+
+const item2 = new Item({
+    name:"Hit the + to add more items"
+});
+
+
+const item3 = new Item({
+    name:"<----- Hit this to delete an item"
+});
+
+const defaultItems = [item1,item2,item3];
+
+
+
 
 app.get("/", function (req,res) {
-
-   // let currentDay = today.getDay();
-
- /*   if(currentDay === 6 || currentDay === 7){
-        day = "Weekend";
-       // res.write("<h1> Weekend </h1>");
-    }
-    else{
-        // res.write("<h1> Weekday </h1>")
-        // res.write("<p> Can't wait for the weekend </p>")
-        // res.send();
-        //day = "Weekday";
-
-       
-      //  res.sendFile(__dirname + "/index.html");
-    }
-*/
-  /*    switch (currentDay) {
-            case 0:
-                day = "Sunday"
-                break;
-            case 1:
-                day = "Monday"
-                break;
-            case 2:
-                day = "Tuesday"
-                break;
-            case 3:
-                day = "Wednesday"
-                break;
-            case 4:
-                day = "Thrusday"
-                break;
-            case 5:
-                day = "Friday"
-                break;
-            case 6:
-                day = "Saturday"
-                break;
-        
-            default:
-            console.log("Error! :( - "+ currentDay)
-                break;
-        }*/
-
-        const day = date.getDate();
-        
-      res.render('list', {listTitle: day, newListItems: items});  //looks for the file 'list' inside the views folder
+    
+    Item.find({},function(err,foundItems){
+       if(foundItems.length === 0){
+        Item.insertMany(defaultItems, function(err){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log("Succesfully saved all items!");
+            }
+        });
+        res.redirect("/");
+       }
+       else{
+          res.render('list', {listTitle: "Today", newListItems: foundItems});  
+       }
+      
+    });
+   
 });
 
 
